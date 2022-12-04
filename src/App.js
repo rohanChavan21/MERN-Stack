6f03be4f6feb0d80ed2c0618e2cc99f1e9394bd6
rohanChavan21 <rohanchavank22@gmail.com>
2022-12-04 08:27:00 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages";
+import SignInPage from "./pages/signin";
 
 export default function App() {
   return (
     <Router>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}  />
+        <Route path="/signin" element={<SignInPage />}  />
+      </Routes>
     </Router>
   );
 }
